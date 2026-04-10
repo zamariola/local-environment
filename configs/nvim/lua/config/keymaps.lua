bd2404efc75601a17fa04ff9ac962e09a45ed169
Leonardo Zamariola <leonardo.zamariola@gmail.com>
2026-04-10 16:01:17 -0300

    Updating lazyvim

--- configs/nvim/lua/config/keymaps.lua
+++ configs/nvim/lua/config/keymaps.lua
@@ -1,7 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-
+--
 -- Grep visual selection with Telescope
 vim.keymap.set("v", "<leader>sg", function()
   -- 1. Grab selection
